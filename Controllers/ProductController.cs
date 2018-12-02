2f8203a61c5425a3b41aef413e34a48d64e30541
HannaM-K <hania.maj1995@gmail.com>
2018-12-02 17:06:28 +0100

    Adding paging to ProductController

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -11,10 +11,17 @@ namespace SportsStore3.Controllers
     {
         //Dependency injection
         private IProductRepository repository;
+        public int PageSize = 4;
+
         public ProductController(IProductRepository repo)
         {
             repository = repo;
         }
-        public ViewResult List() => View(repository.Products);
+
+        public ViewResult List(int productPage = 1) => View(repository.Products
+                                                          .OrderBy(p => p.ProductID)
+                                                          .Skip((productPage - 1) * PageSize)
+                                                          .Take(PageSize));
+
     }
 }
